da8c1306d7144351835570b5e78fe64decd3e6db
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-25 20:50:46 +0200

    add state handling for player name input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import {useState} from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSibmitted] = useState(false);
+
+  const handleChange = (event) => {
+    setEnteredPlayerName(event.target.value);
+  }
+
+  const handleClick = () => {
+    setSibmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
