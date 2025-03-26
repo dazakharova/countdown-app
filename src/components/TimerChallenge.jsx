b104b849c96952f7f59d1819548e4d66f09c2b1a
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-26 20:10:15 +0200

    Show result modal on timer expiration in TimerChallenge

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,8 +1,10 @@
 import {useState, useRef} from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime })
 {
   const timer = useRef()
+  const dialog = useRef()
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -12,6 +14,7 @@ export default function TimerChallenge({ title, targetTime })
 
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal()
     }, targetTime * 1000);
   }
 
@@ -19,17 +22,21 @@ export default function TimerChallenge({ title, targetTime })
     clearTimeout(timer.current)
   }
 
-  return <section className="challenge">
-    <h2>{title}</h2>
-    {timerExpired && <p>You lost!</p>}
-    <p className="challenge-time">
-      {targetTime} second{targetTime > 1 ? 's' : ''}
-    </p>
-    <button onClick={timerStarted ? handleStop : handleStart}>
-      {timerStarted ? 'Stop' : 'Start'} Challenge
-    </button>
-    <p className={timerStarted ? 'active' : undefined}>
-      {timerStarted ? 'Time is running...' : 'Timer inactive'}
-    </p>
-  </section>
+  return (
+      <>
+        {timerExpired && <ResultModal ref={dialog} targetTime={targetTime} result="lost" />}
+        <section className="challenge">
+          <h2>{title}</h2>
+          <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+          </p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+          <p className={timerStarted ? 'active' : undefined}>
+            {timerStarted ? 'Time is running...' : 'Timer inactive'}
+          </p>
+        </section>
+      </>
+  )
 }
