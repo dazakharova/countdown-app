7a4ea8f8a623aa4602a591b6b7280213b006a06d
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-27 19:00:36 +0200

    Render ResultModal using React Portal for improved DOM structure

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {useImperativeHandle, useRef, forwardRef} from 'react'
+import { createPortal } from 'react-dom'
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef()
@@ -15,15 +16,18 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     }
   })
 
-  return <dialog ref={dialog} className="result-modal">
-    {userLost && <h2>You lost</h2>}
-    {!userLost && <h2>Your score {score}</h2>}
-    <p>The target time was <strong>{targetTime} seconds.</strong></p>
-    <p>You stopped the timer with  <strong>{formattedRemainingTime} seconds left.</strong></p>
-    <form method="dalog" onSubmit={onReset}>
-      <button>Close</button>
-    </form>
-  </dialog>
+  return createPortal(
+      <dialog ref={dialog} className="result-modal">
+        {userLost && <h2>You lost</h2>}
+        {!userLost && <h2>Your score {score}</h2>}
+        <p>The target time was <strong>{targetTime} seconds.</strong></p>
+        <p>You stopped the timer with  <strong>{formattedRemainingTime} seconds left.</strong></p>
+        <form method="dalog" onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>,
+      document.getElementById('modal')
+  )
 })
 
 export default ResultModal
