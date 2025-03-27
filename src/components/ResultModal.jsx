ceba127f5031e7ba310a51e1e7d1a23b6bcf3923
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-27 18:46:52 +0200

    Show actual remaining time and implement reset in ResultModal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import {useImperativeHandle, useRef, forwardRef} from 'react'
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef()
+
+  const userLost = remainingTime <= 0
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
 
   useImperativeHandle(ref, () => {
     return {
@@ -12,10 +15,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
   })
 
   return <dialog ref={dialog} className="result-modal">
-    <h2>You {result}</h2>
+    {userLost && <h2>You lost</h2>}
     <p>The target time was <strong>{targetTime} seconds.</strong></p>
-    <p>You stopped the timer with  <strong>X seconds left.</strong></p>
-    <form method="dalog">
+    <p>You stopped the timer with  <strong>{formattedRemainingTime} seconds left.</strong></p>
+    <form method="dalog" onSubmit={onReset}>
       <button>Close</button>
     </form>
   </dialog>
